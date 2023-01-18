c3375eb783cae33289eefdb1f70fd870df04743a
Muizz-DS <47732301+Muizz-DS@users.noreply.github.com>
2023-01-18 15:22:00 +0800

    update on list task

--- TodoList/src/app/model/task.ts
+++ TodoList/src/app/model/task.ts
@@ -1,4 +1,4 @@
 export class Task {
-    id : number = 0;
+    id : string = '';
     task_name : string = '';
 }
--- TodoList/src/app/service/crud.service.ts
+++ TodoList/src/app/service/crud.service.ts
@@ -10,7 +10,7 @@ export class CrudService {
   serviceURL : string ;
 
   constructor(private http : HttpClient) { 
-    this.serviceURL = "http://localhost:3000/tasks"
+    this.serviceURL = "http://localhost:3001/tasks"
   }
 
   addTask(task : Task) : Observable<Task>{
